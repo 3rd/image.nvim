b8b9633c6fc5c5e40360ff615bfbb55d84c5a537
3rd <3rd@users.noreply.github.com>
2023-06-30 01:46:55 +0300

    fix: increase default tmux write delay

--- lua/image/init.lua
+++ lua/image/init.lua
@@ -16,7 +16,7 @@ local default_options = {
   max_width_window_percentage = nil,
   max_height_window_percentage = 50,
   kitty_method = "normal",
-  kitty_tmux_write_delay = 5,
+  kitty_tmux_write_delay = 10,
 }
 
 ---@type State
