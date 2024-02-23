402ff916a17f86fb6542886ad0d553f5d77cf22f
3rd <3rd@users.noreply.github.com>
2024-02-23 23:34:58 +0200

    fix: re-use file descriptor for jpeg check

--- lua/image/utils/magic.lua
+++ lua/image/utils/magic.lua
@@ -11,47 +11,57 @@ local image_signatures = {
   XML = "<?xml",
 }
 
-local function read_file_header(path, numBytes)
-  local file, err = io.open(path, "rb")
-  if not file then return nil, "Failed to open file: " .. err end
-  local content = file:read(numBytes)
-  file:close()
+local function read_file_header(file, num_bytes)
+  if not file then return nil end
+  local current_pos = file:seek()
+  file:seek("set", 0)
+  local content = file:read(num_bytes)
+  file:seek("set", current_pos)
   return content and { content:byte(1, #content) } or nil
 end
 
-local function has_jpeg_end_signature(path)
-  local file, _ = io.open(path, "rb")
+local function has_jpeg_end_signature(file)
   if not file then return false end
   local size = file:seek("end")
-  if size < 2 then
-    file:close()
-    return false
-  end
+  if size < 2 then return false end
   file:seek("set", size - 2)
   local end_signature = file:read(2)
-  file:close()
   return end_signature == "\xFF\xD9", nil
 end
 
 local function is_image(path)
-  local max_bytes = 9
-  local header, err = read_file_header(path, max_bytes)
-  if not header then return false, err end
+  local file, _ = io.open(path, "rb")
+  if not file then return false end
 
+  local max_bytes = 9
+  local header, _ = read_file_header(file, max_bytes)
+  if not header then
+    file:close()
+    return false
+  end
+
+  local is_image_flag = false
   for _, signature in pairs(image_signatures) do
     local bytes = { signature:byte(1, #signature) }
+    local match = true
     for i = 1, #bytes do
-      if header[i] == bytes[i] then
-        if signature == image_signatures.JPEG then
-          if has_jpeg_end_signature(path) then return true, nil end
-        else
-          return true
-        end
+      if header[i] ~= bytes[i] then
+        match = false
+        break
       end
+    end
+    if match then
+      if signature == image_signatures.JPEG then
+        is_image_flag = has_jpeg_end_signature(file)
+      else
+        is_image_flag = true
+      end
+      break
     end
   end
 
-  return false
+  file:close()
+  return is_image_flag
 end
 
 return {
